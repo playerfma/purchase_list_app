0859929327d9666591177da46623a2ff8bbd600b
playerfma <fabio_rp19@hotmail.com>
2024-04-01 21:42:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# purchase_list_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/purchase_list_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.purchase_list_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
